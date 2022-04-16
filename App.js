361babb5e7c96a08ab6d24a41227fd95975e8ab6
adonisjoaquin <93558566+adonisjoaquin@users.noreply.github.com>
2022-04-16 00:05:52 -0400

    updated

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        hello world
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,7 +17,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    paddingTop:100
   },
 });
