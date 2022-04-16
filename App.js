9c99872e9c4f0b82f25280f4db177e9d644a1b69
adonisjoaquin <93558566+adonisjoaquin@users.noreply.github.com>
2022-04-16 01:11:58 -0400

    update

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Ionic from "react-native-vector-icons/Ionicons";
 
 export default function App() {
   return (
@@ -8,10 +9,12 @@ export default function App() {
       <Text>
         hello world
       </Text>
+      <Ionic name="play" style={{fontSize: 20}}/>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
 
 const styles = StyleSheet.create({
   container: {
